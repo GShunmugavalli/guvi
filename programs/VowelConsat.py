ba9a3928ecc242c51450552aed504600cc6a2807
GShunmugavalli <51729075+GShunmugavalli@users.noreply.github.com>
2019-06-12 10:48:33 +0530

    Create VowelConsat.py

---
+++ programs/VowelConsat.py
@@ -0,0 +1,9 @@
+s=input()
+l=['a','e','i','o','u']
+l2=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if s in l:
+  print("Vowel")
+elif s in l2:
+  print("Consonant")
+else:
+  print("invalid")
